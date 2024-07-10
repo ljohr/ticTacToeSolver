093795969ea7871c60d0f97d6886af6a81476f3a
Lilly Ohr <lillyohr@gmail.com>
2024-07-10 09:24:46 -0400

    Create winningCombinations 2d array, create tsconfig

--- tic-tac-toe.ts
+++ tic-tac-toe.ts
@@ -1,5 +1,6 @@
-class Game {
+class TicTacToe {
   private board: string[][];
+  private winningCombinations: Array<Array<[number, number]>>;
 
   constructor() {
     this.board = [
@@ -8,6 +9,68 @@ class Game {
       ["", "", "", ""],
       ["", "", "", ""],
     ];
+    this.winningCombinations = this.generateWinningCombinations();
+    console.log(this.winningCombinations);
+  }
+
+  private generateWinningCombinations(): Array<Array<[number, number]>> {
+    const combinations: Array<Array<[number, number]>> = [];
+
+    // Add all vertical combinations
+    for (let col = 0; col < 4; col++) {
+      combinations.push([
+        [0, col],
+        [1, col],
+        [2, col],
+        [3, col],
+      ]);
+    }
+
+    // Add all horizontal combinations
+    for (let row = 0; row < 4; row++) {
+      combinations.push([
+        [row, 0],
+        [row, 1],
+        [row, 2],
+        [row, 3],
+      ]);
+    }
+
+    // Add diagonal combinations
+    combinations.push([
+      [0, 0],
+      [1, 1],
+      [2, 2],
+      [3, 3],
+    ]);
+    combinations.push([
+      [0, 3],
+      [1, 2],
+      [2, 1],
+      [3, 0],
+    ]);
+
+    // Add 4 corners
+    combinations.push([
+      [0, 0],
+      [0, 3],
+      [3, 0],
+      [3, 3],
+    ]);
+
+    // Add 2x2 Boxes
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+        combinations.push([
+          [row, col],
+          [row, col + 1],
+          [row + 1, col],
+          [row + 1, col + 1],
+        ]);
+      }
+    }
+
+    return combinations;
   }
 
   public checkWinner(): string | null {
@@ -19,6 +82,6 @@ class Game {
   }
 
   public isGameOver(): boolean {
-    return true;
+    return false;
   }
 }
